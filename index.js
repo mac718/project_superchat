af0d46e1b5a1dc03d1fcb0cf3f09c32529ae934d
mac718 <mac718@gmail.com>
2018-11-12 16:30:39 -0500

    Basic websockets setup

--- index.js
+++ index.js
@@ -6,19 +6,33 @@ const redis = require('redis');
 const redisClient = redis.createClient();
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
+const message = require('./lib/post');
 
 app.set('view engine', 'ejs');
 
-app.use(expressLayouts);
-
 app.use('/socket.io', express.static(__dirname + 'node_modules/socket.io-client/dist'));
+
+app.use(expressLayouts);
 
 app.use(express.static(`${__dirname}/public`));  
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+io.on('connection', client => {
+  console.log('hello'); 
+})
+
 app.get('/', (req, res) => {
   res.render('index');
+})
+
+app.post('/posts/new', (req, res) => {
+  let content = req.body.newPostContent;
+  message.newPost(content);
+  redisClient.lindex('posts', 0, (err, value) => {
+    console.log(value);
+    res.redirect('back');
+  })
 })
 
 server.listen(3000);

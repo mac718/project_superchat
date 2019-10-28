9e8d1c695050b17374a79dee1fbf737cecb7e0c1
mac718 <mac718@gmail.com>
2019-10-27 17:01:37 -0700

    add REDISCLOUD config to routes

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const redis = require('redis');
-const redisClient = redis.createClient();
+//var redis = require("redis")
+var url = require('url')
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+redisClient.auth(redisURL.auth.split(":")[1]);
+//const redisClient = redis.createClient();
 const bodyParser = require('body-parser');
 const message = require('./lib/post');
 
